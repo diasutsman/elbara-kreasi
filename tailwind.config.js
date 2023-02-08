7ef5e34d0959c71f3f04b5924788c585e0686eac
Dias Utsman <utsmand91@gmail.com>
2023-02-08 11:43:19 +0700

    Re add color and font extension

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,16 @@ module.exports = {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#A0D995',
+        'secondary': '#6CC4A1',
+        'dark': '#2D0333',
+      },
+      fontFamily: {
+        'sans': ["\"Franklin Gothic Demi\"", 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
