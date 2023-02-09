b79608e057dfccabd8f8663e58396b0c38aa4827
Dias Utsman <utsmand91@gmail.com>
2023-02-09 11:19:29 +0700

    Add js to make dropdown work

--- resources/js/app.js
+++ resources/js/app.js
@@ -17,9 +17,25 @@ const searchBtn = document.getElementById('search-btn')
 const navItems = document.getElementById('nav-items')
 
 searchBtn.addEventListener('click', () => {
-  if(hamburger.classList.contains('hamburger-active')) hamburger.click();
+  if (hamburger.classList.contains('hamburger-active')) hamburger.click();
   search.focus();
   searchForm.classList.toggle('-z-10')
   searchForm.classList.toggle('opacity-0')
   Array.from(searchBtn.children).forEach(child => child.classList.toggle('opacity-0'))
+});
+
+// dropdown
+const contactBtn = document.getElementById('contact-btn');
+const dropdown = document.querySelector('#menu-dropdown')
+contactBtn.addEventListener('click', () => {
+  contactBtn.querySelector('svg').classList.toggle('rotate-180')
+  if (contactBtn.ariaExpanded === 'false') {
+    dropdown.classList.remove(...'opacity-0 scale-95 ease-out duration-100'.split(' '))
+    dropdown.classList.add(...'opacity-100 scale-100 ease-in duration-75'.split(' '))
+    contactBtn.ariaExpanded = 'true'
+  } else {
+    dropdown.classList.remove(...'opacity-100 scale-100 ease-in duration-75'.split(' '))
+    dropdown.classList.add(...'opacity-0 scale-95 ease-out duration-100'.split(' '))
+    contactBtn.ariaExpanded = 'false'
+  }
 })
