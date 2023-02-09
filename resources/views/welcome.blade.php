7b5eb3686a22dde2ad22f91f91863bea93db4425
Dias Utsman <utsmand91@gmail.com>
2023-02-09 13:25:35 +0700

    Make nav sticky

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
     </header>
 
     <!-- Nav Bar -->
-    <div class="w-full p-4 md:flex md:justify-center">
+    <div class="w-full p-4 md:flex md:justify-center sticky top-0 bg-white">
         <div class="flex md:justify-center justify-between flex-wrap mx-auto w-auto relative">
             <button id="hamburger" name="hamburger" type="button" class="block md:hidden relative">
                 <span class="hamburger-line origin-top-left"></span>
