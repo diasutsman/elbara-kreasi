9fbc01f3195591a4a552879e0ea452385c3762c8
Dias Utsman <utsmand91@gmail.com>
2023-03-01 08:46:15 +0800

    Add sun and moon icon to dark mode toggle

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
 <body class="dark:bg-dark-mode">
     <!-- Top Bar -->
     <div class="bg-primary text-onPrimary dark:text-onPrimaryDark text-sm p-4">
-        <div class="container flex justify-between items-center h-full gap-x-4">
+        <div class="container flex justify-between sm:items-center h-full gap-x-4 items-start">
             <div class="flex gap-x-5 gap-y-4 flex-wrap justify-start">
                 <p><a href="https://facebook.com"><i class="bi bi-facebook"></i> Facebook</a></p>
                 <p><a href="https://facebook.com"><i class="bi bi-instagram"></i> Instagram</a></p>
@@ -62,12 +62,16 @@
                         </div>
                     </div>
                 </div>
-                <label class="relative inline-flex items-center cursor-pointer">
-                    <input id="dark-mode-toggle" type="checkbox" class="sr-only peer">
-                    <div
-                        class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
-                    </div>
-                </label>
+                <div class="flex gap-x-2 items-center">
+                    <i class="bi bi-brightness-high-fill"></i>
+                    <label class="relative inline-flex items-center cursor-pointer">
+                        <input id="dark-mode-toggle" type="checkbox" class="sr-only peer">
+                        <div
+                            class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
+                        </div>
+                    </label>
+                    <i class="bi bi-moon-fill"></i>
+                </div>
             </div>
 
         </div>
