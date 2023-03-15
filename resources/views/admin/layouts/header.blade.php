f13b3723d1838896cf40bd19d53ed3ee308886f2
Dias Utsman <utsmand91@gmail.com>
2023-03-15 09:34:29 +0700

    Use alpine js in every changing UI

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -26,32 +26,39 @@
 
             <div class="flex w-full pt-2 content-center justify-between md:w-1/3 md:justify-end">
                 <ul class="list-reset flex justify-between flex-1 md:flex-none items-center">
-                    <li class="flex-1 md:flex-none md:mr-3">
-                        <div class="relative inline-block">
-                            <button onclick="toggleDD('myDropdown')" class="drop-button text-white py-2 px-2"> <span
+                    <li class="flex-1 flex justify-end md:flex-none md:mr-3">
+                        <div class="relative inline-block" x-data="{ open: false }">
+                            <button @click="open = !open" class="drop-button text-white py-2 px-2"> <span
                                     class="pr-2"><i class="em em-robot_face"></i></span> Hi,
                                 {{ auth()->user()->username }} <svg class="h-3 fill-current inline"
                                     xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path
                                         d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
                                 </svg></button>
-                            <div id="myDropdown"
-                                class="dropdownlist absolute bg-gray-800 text-white right-0 mt-3 p-3 overflow-auto z-30 invisible">
-                                <input type="text" class="drop-search p-2 text-gray-600" placeholder="Search.."
-                                    id="myInput" onkeyup="filterDD('myDropdown','myInput')">
-                                <a href="#"
-                                    class="p-2 hover:bg-gray-800 text-white text-sm no-underline hover:no-underline block"><i
-                                        class="fa fa-user fa-fw"></i> Profile</a>
-                                <a href="#"
-                                    class="p-2 hover:bg-gray-800 text-white text-sm no-underline hover:no-underline block"><i
-                                        class="fa fa-cog fa-fw"></i> Settings</a>
-                                <div class="border border-gray-800"></div>
-                                <form action="{{ route('logout') }}" method="POST">
-                                    @csrf
-                                    <button
-                                        class="p-2 hover:bg-gray-800 text-white text-sm no-underline hover:no-underline block"
-                                        type="submit"><i class="fas fa-sign-out-alt fa-fw"></i> Log Out</button>
-                                </form>
+                            <div id="myDropdown" @click.away="open = false"
+                                class="dropdownlist absolute bg-gray-800 text-white right-0 mt-3 p-3 overflow-auto z-30" x-show="open" x-cloak>
+                                <div x-data="{
+                                    search: '',
+                                    show_item(el) {
+                                        return this.search === '' || el.textContent.trim().toLowerCase().includes(this.search.toLowerCase())
+                                    }
+                                }">
+                                    <input type="text" class="drop-search p-2 text-gray-600" placeholder="Search.."
+                                        id="myInput" x-model="search">
+                                    <a href="#"
+                                        class="p-2 hover:bg-gray-800 text-white text-sm no-underline hover:no-underline block" x-show="show_item($refs.profile)" x-ref="profile"><i
+                                            class="fa fa-user fa-fw"></i> Profile</a>
+                                    <a href="#"
+                                        class="p-2 hover:bg-gray-800 text-white text-sm no-underline hover:no-underline block" x-show="show_item($refs.settings)" x-ref="settings"><i
+                                            class="fa fa-cog fa-fw"></i> Settings</a>
+                                    <div class="border border-gray-800"></div>
+                                    <form action="{{ route('logout') }}" method="POST" x-show="show_item($refs.logout)" x-ref="logout">
+                                        @csrf
+                                        <button
+                                            class="p-2 hover:bg-gray-800 text-white text-sm no-underline hover:no-underline block"
+                                            type="submit"><i class="fas fa-sign-out-alt fa-fw"></i> Log Out</button>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </li>
