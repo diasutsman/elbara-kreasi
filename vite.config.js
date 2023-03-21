3889e6e9990ba08a885d168ceab2104d12fff74a
Dias Utsman <utsmand91@gmail.com>
2023-03-21 10:44:29 +0700

    Use absolute path insteand of relative in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                "./resources/**/*.js",
-                "./resources/**/*.css",
+                "resources/js/app.js",
+                "resources/js/categories.js",
+                "resources/js/portfolios.js",
+                "resources/js/products.js",
+                "resources/css/app.css",
             ],
             refresh: true,
         }),
