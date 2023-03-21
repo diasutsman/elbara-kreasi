4c8cd4d2d2307f0f7ad178e52d6ea10cc13b9799
thaariqNasution <Thaariqnst@gmail.com>
2023-03-21 09:10:26 +0700

    update order cards

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -3,16 +3,38 @@
     
 <h1 class="text-4xl text-center my-14">Cara Order</h1>
 
-<div class="flex container mt-16 flex-wrap gap-y-8">
+<div class="flex container flex-wrap gap-x-8 mb-16">
     
-    <div class="row-2 bg-primary justify-items-center p-5">
-
-        <div class="text-center">
-            <p>Masuk/Daftar <br> Opsional</p>
+    <div class="row-2 bg-primary justify-items-center p-5 rounded-xl border-2 border-secondary">
+        <div class="text-center mb-6 text-white">
+            <p>Masuk/Daftar <br> (Opsional)</p>
         </div>
-        <div class="w-40 h-40 bg-grey" ></div>
-
+        <div class="w-60 h-60 bg-grey" ></div>
     </div>
+
+    <img src="{{ asset ('img/arrow-right.svg')}}" class="w-20 h-20 place-self-center" alt="">
+
+    <div class="row-2 bg-primary justify-items-center p-5 rounded-xl border-2 border-secondary">
+        <div class="text-center mb-12 text-white">
+            <p>Pilih Item yang Mau Dibeli</p>
+        </div>
+        <div class="w-60 h-60 bg-grey" ></div>
+    </div>
+
+    <img src="{{ asset ('img/arrow-right.svg')}}" class="w-20 h-20 place-self-center" alt="">
+
+    <div class="row-2 bg-primary justify-items-center p-5 rounded-xl border-2 border-secondary">
+        <div class="text-center mb-12 text-white">
+            <p>Tambahkan Item ke Keranjang</p>
+        </div>
+        <div class="w-60 h-60 bg-grey" ></div>
+    </div>
+
+    <img src="{{ asset ('img/arrow-down.svg')}}" class="w-20 h-20 " alt="">
+
+
+    
+    
 
 </div>
 
