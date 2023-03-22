5a3b4ddbeef41394fa33e5d79432d21dc1e43d91
thaariqNasution <Thaariqnst@gmail.com>
2023-03-22 09:23:27 +0700

    finishing

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -34,7 +34,7 @@
 
 </div>
 
-<div class="container flex flex-row-reverse gap-x-8 mb-16">
+<div class="container flex flex-row-reverse gap-x-8">
         <div class="row-2 bg-primary justify-items-center p-5 rounded-xl border-2 border-secondary mr-2">
             <div class="text-center mb-12 text-white">
                 <p>Klik Tomobl Pesan</p>
@@ -50,7 +50,12 @@
             <div class="w-60 h-60 bg-grey" ></div>
         </div>
 
-    </div>
+</div>
+
+<a href="https://api.whatsapp.com/send?phone=6281234567890&text=Halo,%20Saya%20mau%20order"
+        class="block bg-primaryVariant dark:bg-primaryVariantDark dark:text-dark-mode text-white text-center text-xl p-9 hover:opacity-90 transition-opacity my-32">
+        Ingin Memiliki Packaging Custom Untuk Produk Anda? <span class="underline">Hubungi Kami Sekarang</span>>
+</a> 
 
 
     
