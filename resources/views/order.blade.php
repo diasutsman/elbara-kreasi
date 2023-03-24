26440160b89c531dce9c1263ca1eea7fd3df2677
Dias Utsman <utsmand91@gmail.com>
2023-03-24 11:07:21 +0700

    Layouts order step with grid

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -1,62 +1,65 @@
 @extends('layouts.app')
 @section('content')
-    
-<h1 class="text-4xl text-center my-14">Cara Order</h1>
+    <h1 class="text-4xl text-center my-14">Cara Order</h1>
 
-<div class="flex container flex-wrap gap-x-8">
-    
-    <div class="row-2 bg-primary justify-items-center p-5 rounded-xl border-2 border-secondary">
-        <div class="text-center mb-6 text-white">
-            <p>Masuk/Daftar <br> (Opsional)</p>
-        </div>
-        <div class="w-60 h-60 bg-grey" ></div>
-    </div>
-    <img src="{{ asset ('img/arrow-right.svg')}}" class="w-20 h-20 place-self-center" alt="">
+    <div class="container flex items-center md:items-stretch gap-y-6 flex-col md:grid md:grid-cols-[1fr,90px,1fr] lg:grid-cols-[1fr,90px,1fr,90px,1fr] px-4">
 
-    <div class="row-2 bg-primary justify-items-center p-5 rounded-xl border-2 border-secondary">
-        <div class="text-center mb-12 text-white">
-            <p>Pilih Item yang Mau Dibeli</p>
-        </div>
-        <div class="w-60 h-60 bg-grey" ></div>
-    </div>
-    <img src="{{ asset ('img/arrow-right.svg')}}" class="w-20 h-20 place-self-center" alt="">
-
-    <div class="row-2 bg-primary justify-items-center p-5 rounded-xl border-2 border-secondary">
-        <div class="text-center mb-12 text-white">
-            <p>Tambahkan Item ke Keranjang</p>
-        </div>
-        <div class="w-60 h-60 bg-grey" ></div>
-    </div>
-
-    <div class="container flex justify-end">
-        <img src="{{ asset ('img/arrow-down.svg')}}" class="w-20 h-20 my-8 mr-28" alt="">
-    </div>
-
-</div>
-
-<div class="container flex flex-row-reverse gap-x-8">
-        <div class="row-2 bg-primary justify-items-center p-5 rounded-xl border-2 border-secondary mr-2">
-            <div class="text-center mb-12 text-white">
-                <p>Klik Tomobl Pesan</p>
+        <div class="bg-primary p-5 flex flex-col justify-between gap-y-4 h-[320px] rounded-xl border-2 border-secondary min-w-[400px] md:min-w-0">
+            <div class="text-center text-white">
+                <p>Masuk/Daftar <br> (Opsional)</p>
             </div>
-            <div class="w-60 h-60 bg-grey" ></div>
+            <div class="w-full h-60 bg-grey"></div>
         </div>
-        <img src="{{ asset ('img/arrow-left.svg')}}" class="w-20 h-20 place-self-center" alt="">
 
-        <div class="row-2 bg-primary justify-items-center p-5 rounded-xl border-2 border-secondary">
-            <div class="text-center mb-6 text-white">
+        <img src="{{ asset('img/arrow-right.svg') }}" class="hidden md:block w-10 place-self-center" alt="">
+        <img src="{{ asset('img/arrow-down.svg') }}" class="w-10 md:hidden" alt="">
+
+        <div class="bg-primary p-5 flex flex-col justify-between gap-y-4 h-[320px] rounded-xl border-2 border-secondary min-w-[400px] md:min-w-0">
+            <div class="text-center text-white">
+                <p>Pilih Item yang Mau Dibeli</p>
+            </div>
+            <div class="w-full h-60 bg-grey"></div>
+        </div>
+
+        <div class="hidden md:grid lg:hidden col-span-full justify-self-stretch md:grid-cols-[1fr,90px,1fr] lg:grid-cols-[1fr,90px,1fr,90px,1fr]">
+            <img src="{{ asset('img/arrow-down.svg') }}" class="w-10 col-start-[-2] justify-self-center" alt="">
+        </div>
+
+        <img src="{{ asset('img/arrow-right.svg') }}" class="hidden md:block w-10 place-self-center md:col-start-2 lg:col-start-auto md:rotate-180 lg:rotate-0" alt="">
+        <img src="{{ asset('img/arrow-down.svg') }}" class="w-10 md:hidden" alt="">
+
+        <div class="bg-primary p-5 flex flex-col justify-between gap-y-4 h-[320px] rounded-xl border-2 border-secondary min-w-[400px] md:min-w-0 md:col-start-3 lg:col-start-auto">
+            <div class="text-center text-white">
+                <p>Tambahkan Item ke Keranjang</p>
+            </div>
+            <div class="w-full h-60 bg-grey"></div>
+        </div>
+
+        <div class="col-span-full justify-self-stretch md:grid md:grid-cols-[1fr,90px,1fr] lg:grid-cols-[1fr,90px,1fr,90px,1fr]">
+            <img src="{{ asset('img/arrow-down.svg') }}" class="w-10 md:col-start-1 lg:col-start-[-2] justify-self-center" alt="">
+        </div>
+
+        <div class="bg-primary p-5 flex flex-col justify-between gap-y-4 h-[320px] rounded-xl border-2 border-secondary min-w-[400px] md:min-w-0 lg:col-start-3">
+            <div class="text-center text-white">
                 <p>Pesanan Otomatis <br> Terkirim ke Whatsapp</p>
             </div>
-            <div class="w-60 h-60 bg-grey" ></div>
+            <div class="w-full h-60 bg-grey"></div>
         </div>
 
-</div>
+        <img src="{{ asset('img/arrow-left.svg') }}" class="hidden md:hidden lg:block w-10 place-self-center" alt="">
+        <img src="{{ asset('img/arrow-down.svg') }}" class="w-10 md:hidden" alt="">
 
-<a href="https://api.whatsapp.com/send?phone=6281234567890&text=Halo,%20Saya%20mau%20order"
+        <div class="bg-primary p-5 flex flex-col justify-between gap-y-4 h-[320px] rounded-xl border-2 border-secondary min-w-[400px] md:min-w-0 md:row-start-3 lg:row-start-auto">
+            <div class="text-center text-white">
+                <p>Klik Tombol Pesan</p>
+            </div>
+            <div class="w-full h-60 bg-grey"></div>
+        </div>
+
+    </div>
+
+    <a href="https://api.whatsapp.com/send?phone=6281234567890&text=Halo,%20Saya%20mau%20order"
         class="block bg-primaryVariant dark:bg-primaryVariantDark dark:text-dark-mode text-white text-center text-xl p-9 hover:opacity-90 transition-opacity my-32">
         Ingin Memiliki Packaging Custom Untuk Produk Anda? <span class="underline">Hubungi Kami Sekarang</span>>
-</a> 
-
-
-    
+    </a>
 @endsection
