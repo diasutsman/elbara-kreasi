ad30252e8a729067a8bd5d9140d98ed1b79a6ce9
Dias Utsman <utsmand91@gmail.com>
2023-04-05 13:12:48 +0700

    Add DeletedItem event to Portfolio model

--- app/Models/Portfolio.php
+++ app/Models/Portfolio.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Events\DeletedItem;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Model;
 use Cviebrock\EloquentSluggable\Sluggable;
@@ -12,6 +13,15 @@ class Portfolio extends Model
     use HasFactory, Sluggable;
 
     protected $guarded = ['id', 'created_at', 'updated_at'];
+    
+    /**
+     * The event map for the model.
+     *
+     * @var array
+     */
+    protected $dispatchesEvents = [
+        'deleted' => DeletedItem::class,
+    ];
 
     public function sluggable(): array
     {
