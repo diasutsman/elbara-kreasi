2b3fce32118f3f7585e93f5d9621b61b9005613b
Dias Utsman <utsmand91@gmail.com>
2023-04-05 10:12:22 +0700

    Some changes in products index

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-    <section class="mb-24 mt-1 px-4 text-center dark:text-white">
+    <section class="mb-24 mt-1 px-6 text-center dark:text-white">
 
         <div class="container mx-auto">
             <h2 class="mt-8 text-3xl font-bold">Produk Kami</h2>
@@ -20,26 +20,28 @@
                     </li>
                 @endforeach
             </ul>
-            <div class="products sm:-mr-6">
-                @foreach ($products as $product)
-                    <a class="product {{ $product->category->slug }} block w-full text-left sm:w-1/2 sm:pb-6 sm:pr-6 lg:w-1/3 xl:w-1/4"
-                        href="{{ route('products.show', $product->slug) }}">
-                        <div class="grid h-auto w-full overflow-hidden bg-[#d9d9d9]">
-                            <img class="col-span-full row-span-full block h-full w-full object-center"
-                                src="@if ($product->image) {{ asset('storage/' . $product->image) }}
-                            @else /img/placeholder.webp @endif"
-                                alt="">
-
-                            @if ($product->is_best_seller)
-                                <div class="col-span-full row-span-full p-2">
-                                    <p class="w-fit bg-onPrimary px-3 py-2 text-white">Best Seller</p>
-                                </div>
-                            @endif
-                        </div>
-                        <p class="mt-4 text-base font-bold uppercase">{{ $product->name }}</p>
-                        <p class="mt-1 text-xs text-muted">{{ $product->category->name }}</p>
-                    </a>
-                @endforeach
+            <div class="px-10 sm:px-0">
+                <div class="products sm:-mr-6">
+                    @foreach ($products as $product)
+                        <a class="product {{ $product->category->slug }} block w-full text-left sm:w-1/2 sm:pb-6 sm:pr-6 lg:w-1/3 xl:w-1/4"
+                            href="{{ route('products.show', $product->slug) }}">
+                            <div class="grid h-auto w-full overflow-hidden bg-[#d9d9d9]">
+                                <img class="col-span-full row-span-full block h-full w-full object-center"
+                                    src="@if ($product->image) {{ asset('storage/' . $product->image) }}
+                                @else /img/placeholder.webp @endif"
+                                    alt="">
+    
+                                @if ($product->is_best_seller)
+                                    <div class="col-span-full row-span-full p-2">
+                                        <p class="w-fit bg-onPrimary px-3 py-2 text-white">Best Seller</p>
+                                    </div>
+                                @endif
+                            </div>
+                            <p class="mt-4 text-base font-bold uppercase">{{ $product->name }}</p>
+                            <p class="mt-1 text-xs text-muted">{{ $product->category->name }}</p>
+                        </a>
+                    @endforeach
+                </div>
             </div>
         </div>
     </section>
