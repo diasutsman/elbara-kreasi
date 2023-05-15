e0dc03553429ad17d2758e630ee700b722c2846f
Dias Utsman <utsmand91@gmail.com>
2023-05-15 10:04:23 +0700

    Change seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Log;
 use Cviebrock\EloquentSluggable\Services\SlugService;
+use Exception;
 
 class DatabaseSeeder extends Seeder
 {
@@ -74,41 +75,47 @@ class DatabaseSeeder extends Seeder
             'slug' => 'kalender-notes',
         ]);
 
-        $path = "storage/app/public/images-to-upload";
         
-        $images = collect();
-
-        if ($handle = opendir($path)) {
-            while (false !== ($file = readdir($handle))) {
-                if ('.' === $file) continue;
-                if ('..' === $file) continue;
-
-                $name = Str::before($file, '.');
-                $filename = 'product-images/' . $file;
-                $images->push($file);
-
-                Product::create([
-                    'category_id' => Category::all()->random()->id,
-                    'name' => $name,
-                    'slug' => SlugService::createSlug(Product::class, 'slug', $name),
-                    'description' => $name . ' description',
-                    'is_best_seller' => $this->faker->boolean(70),
-                    'additional_information' => $name . ' additional information',
-                    'image' => $filename,
-                    'price' => $this->faker->numberBetween(1000, 100_000),
-                ]);
+        
+        try {
+            $path = "storage/app/public/images-to-upload";
+            $images = collect();
+            if ($handle = opendir($path)) {
+                while (false !== ($file = readdir($handle))) {
+                    if ('.' === $file) continue;
+                    if ('..' === $file) continue;
+    
+                    $name = Str::before($file, '.');
+                    $filename = 'product-images/' . $file;
+                    $images->push($file);
+    
+                    Product::create([
+                        'category_id' => Category::all()->random()->id,
+                        'name' => $name,
+                        'slug' => SlugService::createSlug(Product::class, 'slug', $name),
+                        'description' => $name . ' description',
+                        'is_best_seller' => $this->faker->boolean(70),
+                        'additional_information' => $name . ' additional information',
+                        'image' => $filename,
+                        'price' => $this->faker->numberBetween(1000, 100_000),
+                    ]);
+                }
+                closedir($handle);
             }
-            closedir($handle);
+            $images->each(function ($image) {
+                Portfolio::create([
+                    'title' => 'Portfolio ' . Str::before($image, '.'),
+                    'slug' => SlugService::createSlug(Portfolio::class, 'slug', 'Portfolio ' . Str::before($image, '.')),
+                    'client' => 'Client ' . Str::before($image, '.'),
+                    'image' => 'portfolio-images/' . $image,
+                    'product_id' => Product::all()->random()->id,
+                ]);
+            });
+        }catch(Exception $e) {
+            Product::factory(20)->create();
+            Portfolio::factory(20)->create();
         }
 
-        $images->each(function ($image) {
-            Portfolio::create([
-                'title' => 'Portfolio ' . Str::before($image, '.'),
-                'slug' => SlugService::createSlug(Portfolio::class, 'slug', 'Portfolio ' . Str::before($image, '.')),
-                'client' => 'Client ' . Str::before($image, '.'),
-                'image' => 'portfolio-images/' . $image,
-                'product_id' => Product::all()->random()->id,
-            ]);
-        });
+        
     }
 }
