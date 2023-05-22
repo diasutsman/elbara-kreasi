d3807a21f5395e947c6ffae91000748e560a0d6a
Dias Utsman <utsmand91@gmail.com>
2023-05-22 07:52:28 +0700

    Update maintenance page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,35 @@
-@extends('layouts.app')
-@section('content')
+<!DOCTYPE html>
+<html class="font-sans" lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon" />
+
+    @vite('resources/css/app.css')
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+
+    @yield('styles')
+
+    {{-- Fonts custom --}}
+    <link rel="stylesheet" href="/css/fonts.css">
+
+    {{-- Swiper --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+
+    <title>Elbara Kreasi Indonesia</title>
+    @livewireStyles
+</head>
+
+<body class="overflow-x-hidden dark:bg-dark-mode">
+    @include('partials.top-bar')
+    @include('partials.header')
+
     <section class="mt-36 mb-52 px-6 dark:text-white">
 
         <div class="container">
@@ -10,7 +40,16 @@
             </div>
         </div>
     </section>
-@endsection
 
-@section('scripts')
-@endsection
+    @include('partials.footer')
+
+    @livewireScripts
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.12.0/dist/cdn.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    @vite('resources/js/app.js')
+    @yield('scripts')
+
+</body>
+
+</html>
