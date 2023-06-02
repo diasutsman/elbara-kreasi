0958211c35efdf1d7e58de7e7e0d895c839c9944
Dias Utsman <utsmand91@gmail.com>
2023-06-02 17:03:58 +0700

    Use process laravel

--- app/Http/Controllers/PrintInvoiceController.php
+++ app/Http/Controllers/PrintInvoiceController.php
@@ -7,16 +7,14 @@ use Illuminate\Http\Request;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Support\Facades\Artisan;
 use Spatie\Browsershot\Browsershot;
-use Symfony\Component\Process\Process;
+use Illuminate\Support\Facades\Process;
 
 class PrintInvoiceController extends Controller
 {
     public function __invoke(Invoice $invoice)
     {
         // shell_exec("source /home/u1574149/nodevenv/elbara-kreasi/16/bin/activate");
-        $process = new Process(['source', '/home/u1574149/nodevenv/elbara-kreasi/16/bin/activate']);
-
-        $process->run();
+        Process::run('source /home/u1574149/nodevenv/elbara-kreasi/16/bin/activate');
 
         $invoiceNumber = 'INV-' . str_pad($invoice->id, 4, '0', STR_PAD_LEFT);
         $invoiceDate = $invoice->due_on->format('j-F-Y');
