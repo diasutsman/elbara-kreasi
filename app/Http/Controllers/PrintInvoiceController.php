fbb5aec17f2d80fd48c8a08f0b33f12a82ce224f
Dias Utsman <utsmand91@gmail.com>
2023-06-02 17:05:20 +0700

    Bisa

--- app/Http/Controllers/PrintInvoiceController.php
+++ app/Http/Controllers/PrintInvoiceController.php
@@ -5,16 +5,16 @@ namespace App\Http\Controllers;
 use App\Models\Invoice;
 use Illuminate\Http\Request;
 use Barryvdh\DomPDF\Facade\Pdf;
-use Illuminate\Support\Facades\Artisan;
 use Spatie\Browsershot\Browsershot;
-use Illuminate\Support\Facades\Process;
+use Symfony\Component\Process\Process;
+use Illuminate\Support\Facades\Artisan;
 
 class PrintInvoiceController extends Controller
 {
     public function __invoke(Invoice $invoice)
     {
         // shell_exec("source /home/u1574149/nodevenv/elbara-kreasi/16/bin/activate");
-        Process::run('source /home/u1574149/nodevenv/elbara-kreasi/16/bin/activate');
+        Process::fromShellCommandline('source /home/u1574149/nodevenv/elbara-kreasi/16/bin/activate');
 
         $invoiceNumber = 'INV-' . str_pad($invoice->id, 4, '0', STR_PAD_LEFT);
         $invoiceDate = $invoice->due_on->format('j-F-Y');
