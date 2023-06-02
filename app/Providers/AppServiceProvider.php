bf1d0e0569bb9c3bf07333787dc4a861edd21982
Dias Utsman <utsmand91@gmail.com>
2023-06-02 09:02:22 +0700

    Remove vite theme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Filament::serving(function () {
             // Using Vite
-            Filament::registerViteTheme('resources/css/filament.css');
+            // Filament::registerViteTheme('resources/css/filament.css');
         });
     }
 }
