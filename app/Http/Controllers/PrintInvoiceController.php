3376b2149e628923263e3c88728f7a31067f0d84
Dias Utsman <utsmand91@gmail.com>
2023-06-02 17:19:48 +0700

    Test

--- app/Http/Controllers/PrintInvoiceController.php
+++ app/Http/Controllers/PrintInvoiceController.php
@@ -13,15 +13,6 @@ class PrintInvoiceController extends Controller
 {
     public function __invoke(Invoice $invoice)
     {
-        // shell_exec("source /home/u1574149/nodevenv/elbara-kreasi/16/bin/activate");
-        $command = ['source', '/home/u1574149/nodevenv/elbara-kreasi/16/bin/activate'];
-
-        $process = new Process($command);
-        $process->run();
-        print_r($process->getErrorOutput());
-
-
-
         $invoiceNumber = 'INV-' . str_pad($invoice->id, 4, '0', STR_PAD_LEFT);
         $invoiceDate = $invoice->due_on->format('j-F-Y');
         $filename = "invoice_{$invoiceNumber}_{$invoiceDate}.pdf";
