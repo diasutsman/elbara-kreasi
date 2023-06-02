0aa2769e72980946c4e832cfc05fa092240e48b8
Dias Utsman <utsmand91@gmail.com>
2023-06-02 17:08:53 +0700

    Bisa dong

--- app/Http/Controllers/PrintInvoiceController.php
+++ app/Http/Controllers/PrintInvoiceController.php
@@ -14,7 +14,13 @@ class PrintInvoiceController extends Controller
     public function __invoke(Invoice $invoice)
     {
         // shell_exec("source /home/u1574149/nodevenv/elbara-kreasi/16/bin/activate");
-        Process::fromShellCommandline('source /home/u1574149/nodevenv/elbara-kreasi/16/bin/activate');
+        $command = ['source', '/home/u1574149/nodevenv/elbara-kreasi/16/bin/activate'];
+
+        $process = new Process($command);
+        $process->run();
+        print_r($process->getErrorOutput());
+
+
 
         $invoiceNumber = 'INV-' . str_pad($invoice->id, 4, '0', STR_PAD_LEFT);
         $invoiceDate = $invoice->due_on->format('j-F-Y');
