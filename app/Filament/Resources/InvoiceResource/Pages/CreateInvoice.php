e15a94c46ab82a77be30e2b2bc1f711d2b85ec3f
Dias Utsman <utsmand91@gmail.com>
2023-06-02 19:24:52 +0700

    Add some footer

--- app/Filament/Resources/InvoiceResource/Pages/CreateInvoice.php
+++ app/Filament/Resources/InvoiceResource/Pages/CreateInvoice.php
@@ -5,8 +5,15 @@ namespace App\Filament\Resources\InvoiceResource\Pages;
 use App\Filament\Resources\InvoiceResource;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Widgets\StatsOverviewWidget\Card;
+use Illuminate\View\View;
 
 class CreateInvoice extends CreateRecord
 {
     protected static string $resource = InvoiceResource::class;
+
+    protected function getFooter(): View
+    {
+        return view('filament.footer');
+    }
 }
---
+++ resources/views/filament/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="text-center">
+    <p class="font-bold">Please create the invoice before adding items</p>
+</footer>
